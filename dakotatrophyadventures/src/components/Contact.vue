a208e3fe64116e9692d0368fdd011632e9e52a63
Tyler Haddix <tyler.hadx@gmail.com>
2022-02-27 21:11:03 -0700

    Added FormSpree for now.

--- dakotatrophyadventures/src/components/Contact.vue
+++ dakotatrophyadventures/src/components/Contact.vue
@@ -7,50 +7,18 @@
         </v-card-title>
         <v-card-text>
 
-                <v-form ref="form"
-                        v-model="valid"
-                        lazy-validation
-                        sx6>
-                    <v-text-field v-model="name"
-                                  :counter="100"
-                                  :rules="nameRules"
-                                  label="Name"
-                                  required></v-text-field>
-
-                    <v-text-field v-model="email"
-                                  :rules="emailRules"
-                                  label="E-mail"
-                                  required></v-text-field>
-
-                    <v-textarea v-model="description"
-                              :items="items"
-                              label="Item"
-                              required></v-textarea>
-
-                  
-
-                    <v-btn :disabled="!valid"
-                           color="success"
-                           @click="validate">
-                        Send
-                    </v-btn>
-
-                </v-form>
-        </v-card-text>
-    </v-card>
-    </v-col></v-row>
-     <v-row><v-col>
-    <v-card :height="windowSize.y" v-if="sentMessage">
-        <v-card-title>
-            <h3 class="headline mb-0">Message Sent</h3>
-        </v-card-title>
-        <v-card-text>
-            <v-layout align-center justify-center>
-                <v-btn color="success"
-                       @click="sentMessage = false">
-                    Back
-                </v-btn>
-            </v-layout>
+           <form id="fs-frm" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/f/meqnkogd" method="post">
+  <fieldset id="fs-frm-inputs">
+    <label for="full-name">Full Name</label>
+    <input type="text" name="name" id="full-name" placeholder="First and Last" required="">
+    <label for="email-address">Email Address</label>
+    <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="">
+    <label for="message">Message</label>
+    <textarea rows="5" name="message" id="message" placeholder="Message" required=""></textarea>
+    <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission">
+  </fieldset>
+  <input type="submit" value="Submit">
+</form>
         </v-card-text>
     </v-card>
     </v-col></v-row>
@@ -68,34 +36,7 @@
             getdata() {
             },
             validate() {
-                if (this.$refs.form.validate()) {
-                    this.sentMessage = false;
-                 var model = {
-                        To: "tyler.hadx@gmail.com",
-            ContactEmail: this.email,
-            Message: this.description,
-            App: "Dakota Trophy Adventures",
-            From: "Contact@DakotaTrophyAdventures.com",
-            Subject: "Dakota Trophy Adventures Contact form.",
-            Name: this.name,
-                 };
-
-
-                 fetch("https://fireemail.gear.host/api/Email",{
-                    method: "POST",
-                    body: JSON.stringify(model),
-                    headers: {
-                        "Content-Type": "application/json"
-                    }   
-                    }).then(function(response) {
-                        return response.json();
-                    }).then(function(data) {
-                      this.sentMessage = true;
-                      console.log(data);
-                    }).catch(function(error) {
-                        console.log(error);
-                    });
-                }
+               
             },
             
         },
@@ -127,3 +68,112 @@
         }
   }
 </script>
+
+<style>
+#fs-frm input,
+#fs-frm select,
+#fs-frm textarea,
+#fs-frm fieldset,
+#fs-frm optgroup,
+#fs-frm label,
+#fs-frm #card-element:disabled {
+  font-family: inherit;
+  font-size: 100%;
+  color: inherit;
+  border: none;
+  border-radius: 0;
+  display: block;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+}
+#fs-frm label,
+#fs-frm legend,
+#fs-frm ::placeholder {
+  font-size: .825rem;
+  margin-bottom: .5rem;
+  padding-top: .2rem;
+  display: flex;
+  align-items: baseline;
+}
+
+/* border, padding, margin, width */
+#fs-frm input,
+#fs-frm select,
+#fs-frm textarea,
+#fs-frm #card-element {
+  border: 1px solid rgba(0,0,0,0.2);
+  background-color: rgba(255,255,255,0.9);
+  padding: .75em 1rem;
+  margin-bottom: 1.5rem;
+}
+#fs-frm input:focus,
+#fs-frm select:focus,
+#fs-frm textarea:focus {
+  background-color: white;
+  outline-style: solid;
+  outline-width: thin;
+  outline-color: gray;
+  outline-offset: -1px;
+}
+#fs-frm [type="text"],
+#fs-frm [type="email"] {
+  width: 100%;
+}
+#fs-frm [type="button"],
+#fs-frm [type="submit"],
+#fs-frm [type="reset"] {
+  width: auto;
+  cursor: pointer;
+  -webkit-appearance: button;
+  -moz-appearance: button;
+  appearance: button;
+}
+#fs-frm [type="button"]:focus,
+#fs-frm [type="submit"]:focus,
+#fs-frm [type="reset"]:focus {
+  outline: none;
+}
+#fs-frm [type="submit"],
+#fs-frm [type="reset"] {
+  margin-bottom: 0;
+}
+#fs-frm select {
+  text-transform: none;
+}
+
+#fs-frm [type="checkbox"] {
+  -webkit-appearance: checkbox;
+  -moz-appearance: checkbox;
+  appearance: checkbox;
+  display: inline-block;
+  width: auto;
+  margin: 0 .5em 0 0 !important;
+}
+
+#fs-frm [type="radio"] {
+  -webkit-appearance: radio;
+  -moz-appearance: radio;
+  appearance: radio;
+}
+
+/* address, locale */
+#fs-frm fieldset.locale input[name="city"],
+#fs-frm fieldset.locale select[name="state"],
+#fs-frm fieldset.locale input[name="postal-code"] {
+  display: inline;
+}
+#fs-frm fieldset.locale input[name="city"] {
+  width: 52%;
+}
+#fs-frm fieldset.locale select[name="state"],
+#fs-frm fieldset.locale input[name="postal-code"] {
+  width: 20%;
+}
+#fs-frm fieldset.locale input[name="city"],
+#fs-frm fieldset.locale select[name="state"] {
+  margin-right: 3%;
+}
+</style>
